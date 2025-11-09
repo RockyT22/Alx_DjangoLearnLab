2b1061208fee80f7fc705e12dd689248531df40f
RockyT22 <201862722+RockyT22@users.noreply.github.com>
2025-11-10 00:04:07 +0200

    Refactor authentication views in views.py
    Removed login, logout views and updated register view.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from .models import Library
@@ -24,8 +23,8 @@ class LibraryListView(ListView):
     template_name = 'relationship_app/library_list.html'
     context_object_name = 'libraries'
 
-# Authentication Views
-def register_view(request):
+# Registration view only (login/logout handled by Django's built-in views)
+def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -35,19 +34,3 @@ def register_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
-
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('relationship_app:list_books')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'relationship_app/login.html', {'form': form})
-
-@login_required
-def logout_view(request):
-    logout(request)
-    return render(request, 'relationship_app/logout.html')
