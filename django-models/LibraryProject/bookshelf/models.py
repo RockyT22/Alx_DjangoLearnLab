71b4fe0ed4cab58717c664858534d7f87e257efa
RockyT22 <201862722+RockyT22@users.noreply.github.com>
2025-11-16 18:27:22 +0200

    Update models.py

--- django-models/LibraryProject/bookshelf/models.py
+++ django-models/LibraryProject/bookshelf/models.py
@@ -1,21 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
-from django.utils.translation import gettext_lazy as _
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, username, date_of_birth, password=None, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
-        if not username:
-            raise ValueError('The Username field must be set')
-        
         email = self.normalize_email(email)
-        user = self.model(
-            email=email,
-            username=username,
-            date_of_birth=date_of_birth,
-            **extra_fields
-        )
+        user = self.model(email=email, username=username, date_of_birth=date_of_birth, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -33,14 +24,10 @@ class CustomUserManager(BaseUserManager):
         return self.create_user(email, username, date_of_birth, password, **extra_fields)
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-    
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'date_of_birth']
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField(upload_to='profile_photos/', blank=True, null=True)
     
     objects = CustomUserManager()
-    
+
     def __str__(self):
-        return self.email
+        return self.username
