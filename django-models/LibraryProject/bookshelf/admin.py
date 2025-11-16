5feea0685353e65a73e5b8fa7c0d232d5a4688e0
RockyT22 <201862722+RockyT22@users.noreply.github.com>
2025-11-16 18:30:08 +0200

    Update admin.py

--- django-models/LibraryProject/bookshelf/admin.py
+++ django-models/LibraryProject/bookshelf/admin.py
@@ -1,25 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.utils.translation import gettext_lazy as _
 from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ['email', 'username', 'date_of_birth', 'is_staff', 'is_active']
-    list_filter = ['is_staff', 'is_active', 'date_joined']
-    fieldsets = (
-        (None, {'fields': ('email', 'username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'date_of_birth', 'profile_photo')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
     )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'username', 'date_of_birth', 'password1', 'password2', 'is_staff', 'is_active')}
-        ),
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
     )
-    search_fields = ['email', 'username']
-    ordering = ['email']
 
 admin.site.register(CustomUser, CustomUserAdmin)
